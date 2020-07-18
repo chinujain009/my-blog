21fc37d503d02bdbf2dfcbe1be5eed4c1ab8b362
Abhishek <chinujain009@gmail.com>
2020-07-18 12:41:39 +0400

    First Commit

---
+++ src/build/precache-manifest.e18c887f09a464082db08c79e18760c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1050675b744c43849979fa2cd77e05a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b8d4a5bfefa998cace5",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "61b81a605b7c806e89e4",
+    "url": "/static/js/2.d4df99f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d4df99f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b8d4a5bfefa998cace5",
+    "url": "/static/js/main.04b7b9b4.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
